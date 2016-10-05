bec999c5af1004f7bf585f89bdf0cefaf252ff25
Seb Insua <me@sebinsua.com>
2016-10-05 17:29:27 +0100

    fix(toResult): support generators, sync and promises

--- src/toResult.js
+++ src/toResult.js
@@ -2,9 +2,21 @@ import generateWrappedFunctionName from './generateWrappedFunctionName'
 import construct from './construct'
 import { Ok, Err } from './Result'
 
+const toOk = construct(Ok)
+const toErr = construct(Err)
+
 export function toResult (fn) {
-  function wrapFunctionWithToResult (...args) {
-    return fn(...args).then(construct(Ok), construct(Err))
+  function * wrapFunctionWithToResult (...args) {
+    try {
+      const out = yield fn(...args)
+      if (!out.then) {
+        return toOk(out)
+      } else {
+        return out.then(toOk, toErr)
+      }
+    } catch (err) {
+      return toErr(err)
+    }
   }
   wrapFunctionWithToResult.displayName = generateWrappedFunctionName(wrapFunctionWithToResult, fn)
   return wrapFunctionWithToResult
